623c1f0adfb031df5652ed9b97b591886b1db8df
DNAWright <dfwrite@gmail.com>
2019-04-04 22:19:30 -0400

    Last commit before submission

--- create.sql
+++ create.sql
@@ -1,3 +1,8 @@
+/* Table with 9 different attributes. Combination of Protein Name and GenBank Accession number used as Primary Key
+    Cannot populate the table with relative pathway for file even if I'm in the directory. Not sure how to fix.
+    Right now I just have it pointing to the exact pathway, which can be changed
+*/
+
 CREATE TABLE Influenza(
     protein VARCHAR,
     accession VARCHAR,
@@ -15,3 +20,10 @@ COPY Influenza
 FROM '/Users/danielwright/BINF6211Project/influenza_final_dat.txt'
 WITH(FORMAT CSV, DELIMITER E'\t', HEADER);
 
+/* Since the accession number is what we will primarily be using to link the databases it is used as a primary key. 
+    However, since there can be different proteins from the same experiment, we needed to include both as a key. I will
+    be creating an index to make searches quicker.
+*/
+
+CREATE INDEX protein_seg_length
+    ON Influenza(protein, seg_length);
